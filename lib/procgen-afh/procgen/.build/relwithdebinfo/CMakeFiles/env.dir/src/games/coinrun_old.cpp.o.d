110645c0e729024999db2dcac76526ef2dfaee24
Pavel C <pavelc.git@posteo.net>
2026-01-07 14:25:28 +0100

    Rename external lib

--- lib/procgen-afh/procgen/.build/relwithdebinfo/CMakeFiles/env.dir/src/games/coinrun_old.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/env.dir/src/games/coinrun_old.cpp.o: \
- /media/linux-data/code/goal-misgen/yrc-bench-fork/lib/procgen-afh/procgen/src/games/coinrun_old.cpp \
- /usr/include/stdc-predef.h
