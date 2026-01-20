373941c807e2e0b8daa204aaca69ef45231f892d
Pavel C <pavelc.git@posteo.net>
2026-01-20 15:27:44 -0800

    Add sync evals script

---
+++ scripts/sync_evals.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SRC_BASE="rnn:/nas/ucb/czempin/data/goal-misgen/experiments/evals"
+DST_BASE="/home/pavel/data/goal-misgen/icml-evals"
+
+ENVS=("maze" "coinrun")
+EXPS=("exp0" "exp1" "exp2")
+
+for env in "${ENVS[@]}"; do
+  for exp in "${EXPS[@]}"; do
+    src_dir="${SRC_BASE}/imcl04_${env}_${exp}"
+    dst_dir="${DST_BASE}/"
+
+    echo "Syncing ${src_dir} -> ${dst_dir}"
+    rsync -av --progress "${src_dir}" "${dst_dir}"
+  done
+done
+
