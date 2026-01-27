9fcb6e58641e416d5ed14dbc1e79b92d56bdda45
Pavel C <pavelc.git@posteo.net>
2026-01-27 14:47:49 -0800

    Update script

--- scripts/sync_evals.sh
+++ scripts/sync_evals.sh
@@ -5,13 +5,13 @@ SRC_BASE="rnn:/nas/ucb/czempin/data/goal-misgen/experiments/evals"
 DST_BASE="/home/pavel/data/goal-misgen/icml-evals"
 
 ENVS=("maze" "coinrun")
-EXPS=("exp0" "exp1" "exp2")
+EXPS=("exp0" "exp1" "exp2" "exp3")
 
 failed_syncs=0
 
 for env in "${ENVS[@]}"; do
   for exp in "${EXPS[@]}"; do
-    src_dir="${SRC_BASE}/imcl04_${env}_${exp}"
+    src_dir="${SRC_BASE}/imcl05-seed-ablation_${env}_${exp}"
     dst_dir="${DST_BASE}/"
 
     echo "Syncing ${src_dir} -> ${dst_dir}"
