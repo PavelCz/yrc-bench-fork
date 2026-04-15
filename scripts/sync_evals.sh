f185e576bdce7809d32dea144bb793fee1a27535
Pavel C <pavelc.git@posteo.net>
2026-04-15 14:10:47 -0700

    Make sync evals prefix an argument

--- scripts/sync_evals.sh
+++ scripts/sync_evals.sh
@@ -4,6 +4,7 @@ set -uo pipefail  # Remove 'e' flag to allow continuing after errors
 SRC_BASE="rnn:/nas/ucb/czempin/data/goal-misgen/experiments/evals"
 DST_BASE="/home/pavel/data/goal-misgen/icml-evals"
 
+PREFIX="${1:-icml04}"
 ENVS=("maze" "coinrun")
 EXPS=("exp0" "exp1" "exp2" "exp3")
 
@@ -11,7 +12,7 @@ failed_syncs=0
 
 for env in "${ENVS[@]}"; do
   for exp in "${EXPS[@]}"; do
-    src_dir="${SRC_BASE}/icml04_${env}_${exp}"
+    src_dir="${SRC_BASE}/${PREFIX}_${env}_${exp}"
     dst_dir="${DST_BASE}/"
 
     echo "Syncing ${src_dir} -> ${dst_dir}"
